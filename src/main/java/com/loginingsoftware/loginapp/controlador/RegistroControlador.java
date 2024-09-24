85eb16887afc8fa73b50e9e0fe78e5ab739b4f9c
SebastianBueno12 <jbuenom1@ucentral.edu.co>
2024-09-24 15:40:44 -0500

    implemente vistas que solo se abren usando ROLE_ADMIN

--- src/main/java/com/loginingsoftware/loginapp/controlador/RegistroControlador.java
+++ src/main/java/com/loginingsoftware/loginapp/controlador/RegistroControlador.java
@@ -19,6 +19,8 @@ public class RegistroControlador {
         return "login";
     }
 
+
+
     @GetMapping("/")
     public String verPaginaDeInicio(Model modelo) {
         modelo.addAttribute("usuarios", servicio.listarUsuarios());
